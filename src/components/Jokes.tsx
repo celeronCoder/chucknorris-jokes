af6ca924c1a6159f20678217c39f978226c2fcf3
celeronCoder <khushalbhardwaj0111@gmail.com>
2022-03-26 23:42:16 +0530

    chore: pretty by prettier

--- src/components/Jokes.tsx
+++ src/components/Jokes.tsx
@@ -1,20 +1,18 @@
-import { Container, createStyles, SimpleGrid } from '@mantine/core';
-import JokeCard from './JokeCard';
+import { Container, createStyles, SimpleGrid } from "@mantine/core";
+import JokeCard from "./JokeCard";
 
-const useStyles = createStyles(theme => {
-	const dark = theme.colorScheme === 'dark';
-	return {
-		container: {
-			display: 'flex',
-			alignItems: 'center',
-			justifyContent: 'center',
-			padding: '0.5em',
-		},
-		grid: {
-			flex: 1
-		}
-	}
-})
+// eslint-disable-next-line no-unused-vars
+const useStyles = createStyles((_theme) => ({
+	container: {
+		display: "flex",
+		alignItems: "center",
+		justifyContent: "center",
+		padding: "0.5em",
+	},
+	grid: {
+		flex: 1,
+	},
+}));
 
 interface CreateRangeProps {
 	from?: number;
@@ -23,25 +21,30 @@ interface CreateRangeProps {
 	length?: number;
 }
 
-function createRange({ from = 0, to, step = 1, length = Math.ceil((to - from) / step)} : CreateRangeProps) {
-	return Array.from({length}, (_, i) => from + i * step)
+function createRange({
+	from = 0,
+	to,
+	step = 1,
+	length = Math.ceil((to - from) / step),
+}: CreateRangeProps) {
+	return Array.from({ length }, (_, i) => from + i * step);
 }
 
 export default function Jokes() {
 	const { classes } = useStyles();
 	return (
-		<Container className={classes.container} >
-			<SimpleGrid 
-				cols={3} 
-				spacing="lg" 
+		<Container className={classes.container}>
+			<SimpleGrid
+				cols={3}
+				spacing="lg"
 				className={classes.grid}
 				breakpoints={[
-					{ maxWidth: 'md', cols: 3, spacing: 'lg' },
-					{ maxWidth: 'sm', cols: 2, spacing: 'md' },
-					{ maxWidth: 'xs', cols: 1, spacing: 'sm' },
-				]} 
+					{ maxWidth: "md", cols: 3, spacing: "lg" },
+					{ maxWidth: "sm", cols: 2, spacing: "md" },
+					{ maxWidth: "xs", cols: 1, spacing: "sm" },
+				]}
 			>
-				{createRange({to: 15}).map(() => (
+				{createRange({ to: 15 }).map(() => (
 					<JokeCard />
 				))}
 			</SimpleGrid>
--- src/components/SegmentToggle.tsx
+++ src/components/SegmentToggle.tsx
@@ -1,36 +1,41 @@
-import React from 'react';
-import { useMantineColorScheme, SegmentedControl, Group, Center, Box } from '@mantine/core';
+import {
+	useMantineColorScheme,
+	SegmentedControl,
+	Group,
+	Center,
+	Box,
+} from "@mantine/core";
 import { FiSun, FiMoon } from "react-icons/fi";
 
-export function SegmentedToggle() {
-  const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+export default function SegmentedToggle() {
+	const { colorScheme, toggleColorScheme } = useMantineColorScheme();
 
-  return (
-    <Group position="center" my="xl">
-      <SegmentedControl
-        value={colorScheme}
-        onChange={() => toggleColorScheme()}
-        data={[
-          {
-            value: 'light',
-            label: (
-              <Center>
-                <FiSun size={16} />
-                <Box ml={10}>Light</Box>
-              </Center>
-            ),
-          },
-          {
-            value: 'dark',
-            label: (
-              <Center>
-                <FiMoon size={16} />
-                <Box ml={10}>Dark</Box>
-              </Center>
-            ),
-          },
-        ]}
-      />
-    </Group>
-  );
+	return (
+		<Group position="center" my="xl">
+			<SegmentedControl
+				value={colorScheme}
+				onChange={() => toggleColorScheme()}
+				data={[
+					{
+						value: "light",
+						label: (
+							<Center>
+								<FiSun size={16} />
+								<Box ml={10}>Light</Box>
+							</Center>
+						),
+					},
+					{
+						value: "dark",
+						label: (
+							<Center>
+								<FiMoon size={16} />
+								<Box ml={10}>Dark</Box>
+							</Center>
+						),
+					},
+				]}
+			/>
+		</Group>
+	);
 }
